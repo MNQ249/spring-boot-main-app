31f98d8fa253957f0be7b373f9a00b725beb2042
Mesfer <mesfer.vip@gmail.com>
2019-12-29 19:25:42 +0300

    crate a new entity student

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -5,7 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Student {
@@ -15,12 +17,45 @@ public class Student {
     private long id;
 
     @NotNull
+    @Size(min = 2, max = 10)
     private String name;
 
     @NotNull
     private int age;
 
     @NotNull
+    @Max(4)
     private int GPA;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGPA() {
+        return GPA;
+    }
+
+    public void setGPA(int GPA) {
+        this.GPA = GPA;
+    }
 }
